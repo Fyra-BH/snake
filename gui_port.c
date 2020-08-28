55efd9d593c456b47c858be159cbfc21a54447be
Fyra-BH <58434746+Fyra-BH@users.noreply.github.com>
2020-08-28 17:23:49 +0800

    Add files via upload

---
+++ gui_port.c
@@ -0,0 +1,27 @@
+/**
+ * 
+ * 
+*/
+
+#include "gui_port.h"
+
+
+
+void gui_test()
+{
+    
+}
+void gui_clear(char p)
+{
+  
+}
+
+void gui_flush()
+{
+   
+}
+
+void gui_draw_rect(int x1,int y1,int x2,int y2,int color)
+{
+     Glib_FilledRectangle(x1,y1,x2,y2,color);
+}
